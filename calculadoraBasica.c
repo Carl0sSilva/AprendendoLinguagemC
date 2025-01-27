c368d4c579ee7f202f71adc06b86bf5b180a083c
Carlos Silva <carloscustodio069@gmail.com>
2025-01-27 13:04:38 -0300

    Adicionando 4 operações básicas

---
+++ calculadoraBasica.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+double soma(double n1, double n2) { return n1 + n2; }
+
+double subtracao(double n1, double n2) { return n1 - n2; }
+
+double multiplicacao(double n1, double n2) { return n1 * n2; }
+
+double divisao(double n1, double n2) { return n1 / n2; }
